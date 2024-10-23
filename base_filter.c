d73686281de592aa8faf918c5b815bbbb3efa4d7
Jérôme Gorin <gorinje@gmail.com>
2024-10-23 15:20:04 +0200

    test

--- base_filter.c
+++ base_filter.c
@@ -14,6 +14,7 @@ static void base_filter_finalize(GF_Filter *filter)
 {
 	//peform any finalyze routine needed, including potential free in the filter context
 	//if not needed, set the filter_finalize to NULL
+	
 }
 static GF_Err base_filter_process(GF_Filter *filter)
 {
